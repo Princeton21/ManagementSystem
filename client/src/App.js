ac37c3a5c92cd7a44cf7b2a55387adb8b153c20b
Princeton D'souza <princeton2102@gmail.com>
2022-11-09 09:56:51 +0530

    c and r of crud done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+
+  const [newItemName, setNewItemName] = useState("");
+
+  const [shopList, setShopList] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      // console.log(response);
+      setShopList(response.data);
+    });
+  }, []);
+
+  const addToList = () => {
+    // console.log(name + price + quantity);
+    Axios.post("http://localhost:3001/insert", {
+      name: name,
+      price: price,
+      quantity: quantity,
+    });
+  };
+  const updateItem = () => {
+    Axios.put("http://localhost:3001/update", {
+      name: name,
+      newName: newItemName,
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shopping list</h1>
+      <label>Item name</label>
+      <input
+        type="text"
+        placeholder="Enter an item"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label>Item price</label>
+      <input
+        type="number"
+        placeholder="Enter the price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <label>Item quantity</label>
+      <input
+        type="number"
+        placeholder="Enter the quantity"
+        onChange={(e) => setQuantity(e.target.value)}
+      />
+      <button onClick={addToList}>Add to the list</button>
+      <hr />
+      <h2>Shopping list</h2>
+      {shopList.map((val, key) => {
+        return (
+          <div key={key}>
+            <h3>{val.itemName}</h3>
+            <p>Price: {val.itemPrice}</p>
+            <p>Quantity: {val.itemQuantity}</p>
+            <input
+              type="text"
+              placeholder="Enter new food name..."
+              onChange = {(e) => {setNewItemName(e.target.value)}}
+            />
+            <button onClick={() => updateItem(val._id)}>Update</button>
+            <button>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
 const FoodModel = require("./models/Food");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://newuser:crud123@information.mp6xwst.mongodb.net/shopping_list?retryWrites=true&w=majority",
@@ -13,17 +15,44 @@ mongoose.connect(
   }
 );
 
-app.get("/", async (req, res) => {
-    const food = new FoodModel({ itemName: "Pizza", itemPrice: 2.99, itemQuantity: 1 });
-    try {
-      await food.save();
-      res.send("Item has been saved to the database");
-    } catch (err) {
-      console.log(err);
-    }
+app.put("/update", async (req, res) => {
+  const newFoodName = req.body.newFoodName;
+  const id = req.body.id;
+  try {
+    FoodModel.findById(id, (updatedFood) => {
+      updatedFood.foodName = newFoodName
+      updatedFood.save();
+    })
+  } catch (err) {
+    console.log(err);
+  }
+  
+
 });
-
-
+app.post("/insert", async (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+  const food = new FoodModel({
+    itemName: name,
+    itemPrice: price,
+    itemQuantity: quantity,
+  });
+  try {
+    await food.save();
+    res.send("Item has been saved to the database");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  })
+});
 
 app.listen(3001, () => {
   console.log("Server running on port 3001...");
