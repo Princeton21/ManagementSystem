a1bf9187ad54d1b9d08b667b8dc732b447f17df2
Princeton D'souza <princeton2102@gmail.com>
2022-11-09 16:11:28 +0530

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
       // console.log(response);
       setShopList(response.data);
     });
-  },[]);
+  }, []);
 
   const addToList = () => {
     // console.log(name + price + quantity);
@@ -30,51 +30,57 @@ function App() {
       id: id,
       newItemName: newItemName,
     });
-  }
+  };
   const deleteItem = (id) => {
     Axios.delete(`http://localhost:3001/delete/${id}`);
-  }
+  };
 
   return (
     <div className="App">
       <h1>Shopping list</h1>
-      <label>Item name</label>
-      <input
-        type="text"
-        placeholder="Enter an item"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label>Item price</label>
-      <input
-        type="number"
-        placeholder="Enter the price"
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <label>Item quantity</label>
-      <input
-        type="number"
-        placeholder="Enter the quantity"
-        onChange={(e) => setQuantity(e.target.value)}
-      />
-      <button onClick={addToList}>Add to the list</button>
+      <div className="card-container">
+        <label>Item name</label>
+        <input
+          type="text"
+          placeholder="Enter an item"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Item price</label>
+        <input
+          type="number"
+          placeholder="Enter the price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <label>Item quantity</label>
+        <input
+          type="number"
+          placeholder="Enter the quantity"
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <button onClick={addToList}>Add to the list</button>
+      </div>
       <hr />
-      <h2>Shopping list</h2>
-      {shopList.map((val, key) => {
-        return (
-          <div key={key}>
-            <h3>{val.itemName}</h3>
-            <p>Price: {val.itemPrice}</p>
-            <p>Quantity: {val.itemQuantity}</p>
-            <input
-              type="text"
-              placeholder="Enter new food name..."
-              onChange = {(e) => {setNewItemName(e.target.value)}}
-            />
-            <button onClick={() => updateItem(val._id)}>Update</button>
-            <button onClick={() => deleteItem(val._id)}>Delete</button>
-          </div>
-        );
-      })}
+      <h1>Cart</h1>
+      <div className="grid_container">
+        {shopList.map((val, key) => {
+          return (
+            <div className="render_card" key={key}>
+              <h3>{val.itemName}</h3>
+              <p>Price: {val.itemPrice}</p>
+              <p>Quantity: {val.itemQuantity}</p>
+              <input
+                type="text"
+                placeholder="Enter new item name..."
+                onChange={(e) => {
+                  setNewItemName(e.target.value);
+                }}
+              />
+              <button onClick={() => updateItem(val._id)}>Update</button>
+              <button onClick={() => deleteItem(val._id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
